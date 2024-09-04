49293854dcc37642663f26f0c8ca94f3c386a02f
efranzolin <efranzolin.ext@ecocerved.it>
2024-09-04 14:51:27 +0200

    Conclusione Get Started Angular 15

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import {Product, products } from '../products';
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,9 +14,22 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private cartService: CartService){}
 
   ngOnInit() {
-    
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //Find the product that correspond with the id provided route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
+
+  addToCart(){
+    if(products != null){
+      this.cartService.addToCart(this.product as Product);
+      window.alert('Your product has been added to the cart!');
+    }
+  }
+
 }
